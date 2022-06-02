6ab32572e955402c751d5023eb03c2cfd5506d56
OlegKravinskyy <kravinskyy@gmail.com>
2022-06-02 20:14:36 +0300

    footer adapted

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,17 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('.menu-open-btn'),
-    closeMenuBtn: document.querySelector('.menu-close-btn'),
-    menu: document.querySelector('.mob-menu-top'),
-    body: document.querySelector('body'),
-  };
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
-  }
-})();
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('.menu-open-btn'),
+//     closeMenuBtn: document.querySelector('.menu-close-btn'),
+//     menu: document.querySelector('.mob-menu-top'),
+//     body: document.querySelector('body'),
+//   };
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// })();
 
 // (() => {
 //   const refs = {
@@ -27,3 +27,20 @@
 //     refs.body.classList.toggle('no-scroll');
 //   }
 // })();
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
+  };
+  console.log('refs :>> ', refs);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  function toggleMenu() {
+    console.log('111 :>> ', 111);
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
